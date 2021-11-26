4c6c0c9f5aaa3ff63d87f5ea5ff9c9da3d50631d
RegisSalesRA <regismsales@gmail.com>
2021-11-26 03:10:24 -0300

    Delete app

--- django_forms/admin.py
+++
@@ -1,6 +0,0 @@
-from django_forms.models import CadastroForm, FastCadastro
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(CadastroForm)
-admin.site.register(FastCadastro)
--- django_forms/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoFormsConfig(AppConfig):
-    name = 'django_forms'
