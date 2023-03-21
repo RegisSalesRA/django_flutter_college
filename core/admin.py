31eb3f58a8d592a795a15d0203bfee093245d185
RegisSalesRA <regismsales@gmail.com>
2023-03-21 10:21:17 -0300

    delete old project

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from core.models import Cadastro, CadastroFoto
-
-# Register your models here.
-
-admin.site.register(Cadastro)
-admin.site.register(CadastroFoto)
--- core/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path,include
-
-urlpatterns = [
-    path('v1/', include('core.v1.urls'))
-]
