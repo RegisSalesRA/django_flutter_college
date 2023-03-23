38352d9282b8a6901234a842969fd9d051ed9007
RegisSalesRA <regismsales@gmail.com>
2023-03-22 22:00:11 -0300

    config settings e dotenv

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from server.settings import base
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+    
+    if base.DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'server.settings.local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                               'server.settings.production')        
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- server/settings/base.py
+++ server/settings/base.py
@@ -1,23 +1,19 @@
 from pathlib import Path
 from datetime import timedelta
+import os
+import dotenv
+dotenv.load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!hj3+4f!mn^1^k97ne9olft=^ddygvl8ye^2_5tunry9r@ssw-"
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
