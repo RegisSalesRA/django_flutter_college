b3da790607174f3aeb0913b7759b059943e1c91b
RegisSalesRA <regismsales@gmail.com>
2023-03-26 17:19:13 -0300

    updade motels

--- core/v1/models/school_models.py
+++ core/v1/models/school_models.py
@@ -1,34 +1,36 @@
 from django.db import models
 from core.v1.models.auth_models import Student, Teacher
 
+    
+class Semester(models.Model):
+    SEMESTER_DICT = (
+        ('semester_1', 'Semester 1'),
+        ('semester_2', 'Semester 2'),
+        ('semester_3', 'Semester 3'),
+        ('semester_4', 'Semester 4'),  
+    )
+    semester =  models.CharField(max_length=10, choices=SEMESTER_DICT)
+
+    createAt = models.DateTimeField(auto_now_add=True, auto_created=True)
+    updateAt = models.DateTimeField(auto_now=True, auto_created=True)
+
+    def __str__(self):
+        return self.semester
+
+
 class Discipline(models.Model):
     name = models.CharField(max_length=100)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    ano = models.CharField(max_length=4)
     student = models.ManyToManyField(Student)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+
     createAt = models.DateTimeField(auto_now_add=True, auto_created=True)
     updateAt = models.DateTimeField(auto_now=True, auto_created=True)    
     
     def __str__(self):
         return self.name
     
-class Semester(models.Model):
-    SEMESTER_DICT = (
-        ('semester_1', 'Semester_1'),
-        ('semester_2', 'Semester_2'),
-        ('semester_3', 'Semester_3'),
-        ('semester_4', 'Semester_4'),  
-    )
-    semester =  models.CharField(max_length=10, choices=SEMESTER_DICT)
-    year = models.CharField(max_length=4)
-    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
-
-    createAt = models.DateTimeField(auto_now_add=True, auto_created=True)
-    updateAt = models.DateTimeField(auto_now=True, auto_created=True)
-
-    def __str__(self):
-        return f"semester - {self.semester}"
-
-
 class Scores(models.Model):
     aluno = models.ForeignKey(Student, on_delete=models.CASCADE)
     discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
