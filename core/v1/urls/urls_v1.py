f8321c699ccaf3b12ade6b6283dc62ec5dd31c10
RegisSalesRA <regismsales@gmail.com>
2023-04-13 22:22:18 -0300

    backend refacotry urls

--- core/v1/urls/urls_v1.py
+++ core/v1/urls/urls_v1.py
@@ -24,20 +24,21 @@ from core.v1.views.college_views import (
 )
 
 urlpatterns = [
-    # College
+    # Admin
     path("discipline/", DisciplineListView.as_view()),
     path("discipline_post/", DisciplineCreateView.as_view()),
-    path("discipline_student/", DisciplinyByStudent.as_view()),
-    path("discipline_chose/", ChoseDisciplineByStudent.as_view()),
-    path("discipline_left_student/", DisciplineListLeftStudent.as_view()),
-    path("discipline_teacher/", DisciplineListByTeacher.as_view()),
     path("discipline/<int:pk>/", DisciplineRetrieveUpdateDestroyView.as_view()),
     path("semester/", SemesterListCreateView.as_view()),
     path("semester/<int:pk>/", SemesterRetrieveUpdateDestroyView.as_view()),
     path("score/", ScoresListView.as_view()),
-    path("score_add/", ScoresCreateView.as_view()),
-    path("score_add_student/", InsertScoreToStudent.as_view()),
     path("score/<int:pk>/", ScoresRetrieveUpdateDestroyView.as_view()),
+    path("score_add/", ScoresCreateView.as_view()),
+    # College
+    path("discipline_student/", DisciplinyByStudent.as_view()),
+    path("discipline_chose/", ChoseDisciplineByStudent.as_view()),
+    path("discipline_left_student/", DisciplineListLeftStudent.as_view()),
+    path("discipline_teacher/", DisciplineListByTeacher.as_view()),
+    path("score_add_student/", InsertScoreToStudent.as_view()),
     # Auth
     path("signup/student/", StudentSignupView.as_view()),
     path("student/", StudentListCreateView.as_view()),
